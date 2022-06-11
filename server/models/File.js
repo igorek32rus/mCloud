6983d404e0ca5ec4ff0dc499ffe4f11488863d54
Игорь <igor.elizaroff@yandex.ru>
2022-06-11 23:02:36 +0300

    Created model of file

---
+++ server/models/File.js
@@ -0,0 +1,13 @@
+const {Schema, model, ObjectId} = require("mongoose")
+
+const File = new Schema({
+    name: {type: String, required: true},
+    type: {type: String, required: true},
+    accessLink: {type: String},
+    size: {type: Number, default: 0},
+    parent: {type: ObjectId, ref: 'File'},
+    childs: [{type: ObjectId, ref: 'File'}],
+    user: {type: ObjectId, ref: 'User'}
+})
+
+module.exports = model('File', File)
