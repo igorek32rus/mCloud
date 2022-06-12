da9cb0c90993bafc0b8e7ae524a55658a27f01d9
Игорь <igor.elizaroff@yandex.ru>
2022-06-12 04:14:01 +0300

    Added date to model of file

--- server/models/File.js
+++ server/models/File.js
@@ -5,6 +5,7 @@ const File = new Schema({
     type: {type: String, required: true},
     accessLink: {type: String},
     size: {type: Number, default: 0},
+    date: {type: Date, default: new Date()},
     parent: {type: ObjectId, ref: 'File'},
     childs: [{type: ObjectId, ref: 'File'}],
     user: {type: ObjectId, ref: 'User'}
