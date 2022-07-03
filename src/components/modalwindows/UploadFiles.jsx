c68827083f8c83e17efc80c76382190dff1be8a5
Игорь <igor.elizaroff@yandex.ru>
2022-07-03 20:51:44 +0300

    Fixed display of list upload files

--- src/components/modalwindows/UploadFiles.jsx
+++ src/components/modalwindows/UploadFiles.jsx
@@ -15,16 +15,18 @@ function UploadFiles(props) {
     return (
         <div className="modal_content" style={{display: 'flex', flexDirection: 'column'}}>
             <h1>Список файлов для загрузки</h1>
-            <table>
-                <tbody>
-                    <tr>
-                        <th>Имя файла</th>
-                        <th>Размер</th>
-                        <th></th>
-                    </tr>
-                    {Array.from(props.files).map((file) => <tr key={new Date() - file.size}><td>{file.name}</td><td>{file.size}</td><td className="removeUpload">&times;</td></tr>)}
-                </tbody>
-            </table>
+            <div className="list_upload">
+                <table>
+                    <tbody>
+                        <tr>
+                            <th>Имя файла</th>
+                            <th>Размер</th>
+                            <th></th>
+                        </tr>
+                        {Array.from(props.files).map((file) => <tr key={new Date() - file.size}><td>{file.name}</td><td>{file.size}</td><td className="removeUpload">&times;</td></tr>)}
+                    </tbody>
+                </table>
+            </div>
             <Button click={handleUploadFilesBtn} style={{margin: 0}}>Загрузить</Button>
         </div>
     )
