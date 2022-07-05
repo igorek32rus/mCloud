01e0396fc834e1f19d364fbe0f1ec090aa42cb39
Игорь <igor.elizaroff@yandex.ru>
2022-07-05 17:07:23 +0300

    Added function delete file for upload in modal

--- src/components/modalwindows/UploadFiles.jsx
+++ src/components/modalwindows/UploadFiles.jsx
@@ -1,13 +1,24 @@
-import React, {useContext} from "react";
+import React, {useContext, useState} from "react";
 import Button from "../UI/button/Button";
 import { ModalContext } from "../../Context";
 
 function UploadFiles(props) {
+    const [uploadFiles, setUploadFiles] = useState(props.files)
     const {setModal} = useContext(ModalContext)
 
     const handleUploadFilesBtn = () => {
-        props.uploadFiles(props.files)
+        props.uploadFiles(uploadFiles)
         setModal(false)
+    }
+
+    const handleRemoveFile = (num) => {
+        let copyUploadFiles = uploadFiles.slice(0)
+        copyUploadFiles.splice(num, 1)
+        setUploadFiles(copyUploadFiles)
+
+        if (copyUploadFiles.length === 0) {
+            setModal(false)
+        }
     }
 
     return (
@@ -21,7 +32,8 @@ function UploadFiles(props) {
                             <th>Размер</th>
                             <th></th>
                         </tr>
-                        {Array.from(props.files).map((file) => <tr key={new Date() - file.size}><td>{file.name}</td><td>{file.size}</td><td className="removeUpload">&times;</td></tr>)}
+                        {uploadFiles.map((file, i) => <tr key={i}><td>{file.name}</td><td>{file.size}</td><td onClick={() => handleRemoveFile(i)} className="removeUpload">&times;</td></tr> )}
+                        {/* {Array.from(props.files).map((file) => <tr key={new Date() - file.size}><td>{file.name}</td><td>{file.size}</td><td className="removeUpload">&times;</td></tr>)} */}
                     </tbody>
                 </table>
             </div>
