7a419a88eabf3226742e0e9212052747bfa69ace
igorek32rus <igor.elizaroff@yandex.ru>
2022-08-13 23:38:13 +0300

    Added function - copy to clipboard (modal - share)

--- src/components/modalwindows/Share.jsx
+++ src/components/modalwindows/Share.jsx
@@ -1,15 +1,30 @@
-import React from "react"
-import Button from "../UI/button/Button"
+import React, {useContext} from "react"
+import linkImg from "../../images/link.svg"
+import { ModalContext, NotifyContext } from "../../Context"
 
 function Share({items}) {
+    const {createNotification} = useContext(NotifyContext)
+    const {closeModal} = useContext(ModalContext)
+
+    const text = "test text"
+
     const handleShareBtn = () => {
-        console.log('Copied');
+        navigator.clipboard.writeText(text).then(() => {
+            createNotification({
+                title: `Буфер обмена`, 
+                message: `Ссылка успешно скопирована в буфер обмена`
+            })
+        }, (err) => {
+            console.error('Async: Could not copy text: ', err);
+        });
     }
 
     return (
         <>
-            <input type="text" value={"link for " + items[0].name} onChange={(e) => e.preventDefault()} />
-            <Button click={handleShareBtn} style={{margin: 0}}>Скопировать ссылку</Button>
+            <div style={{display: 'flex', alignItems: "center"}}>
+                <input type="text" value={"link for " + items[0].name} onChange={(e) => e.preventDefault()} />
+                <div className="imgBtn" style={{backgroundImage: `url(${linkImg})`, marginLeft: 5}} onClick={handleShareBtn}></div>
+            </div>
         </>
     )
 }
