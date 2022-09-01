7710afdce9ee3dff75d9a3c4a47d8b1f32d94adf
Игорь <igor.elizaroff@yandex.ru>
2022-09-01 23:53:45 +0300

    Updated link of logo

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,11 +1,14 @@
-import React from 'react';
+import React from 'react'
+import { Link } from 'react-router-dom'
 
 function Logo() {
     return (
-        <a className="logo" href="/">
-            <div className="logo-img"></div>
-            <span className="thin">m</span><span className="bold">Cloud</span>
-        </a>
+        <Link to="/files">
+            <div className="logo" href="#">
+                <div className="logo-img"></div>
+                <span className="thin">m</span><span className="bold">Cloud</span>
+            </div>
+        </Link>
     )
 }
 
