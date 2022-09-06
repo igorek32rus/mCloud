8613afd006ea37c1154e64126039e365c3cd5565
Игорь <igor.elizaroff@yandex.ru>
2022-09-06 05:29:03 +0300

    Changed path. useHistory -> Link

--- src/components/Path.jsx
+++ src/components/Path.jsx
@@ -1,30 +1,27 @@
 import React from "react"
-import { useHistory, useParams } from "react-router-dom"
+import { useParams, Link } from "react-router-dom"
 
 import '../styles/Path.css'
 
 function Path({path}) {
-    const history = useHistory()
     const {category} = useParams()
-
-    const handlerClick = (dir) => {
-        history.push(`/files/${category}/${dir._id}`)
-    }
     
     return (
         <nav className="path">
             <ul>
                 { path.map(item => (
-                    <li key={item?._id} onClick={() => handlerClick(item)}>
+                    <li key={item?._id}>
                         { item?.parent ? (
                             <>
                                 <div className="delimiter">&#10140;</div>
-                                <div className="link">{item.name}</div>
+                                <Link to={"/files/" + category + "/" + item._id}><div className="link">{item.name}</div></Link>
                             </>
                         ) : (
-                            <div className="link" style={{padding: 3}}>
-                                <div className="home">Главная</div>
-                            </div>
+                            <Link to={"/files/" + category + "/" + item._id}>
+                                <div className="link" style={{padding: 3}}>
+                                    <div className="home">Главная</div>
+                                </div>
+                            </Link>
                         ) }
                         
                     </li>
