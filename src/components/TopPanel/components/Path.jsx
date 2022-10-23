e75fc2b766d1010082c460e25bbdbbed5ad5ef97
Igor Elizaroff <igor.elizaroff@yandex.ru>
2022-10-24 02:42:48 +0300

    Added scroll for path

--- src/components/TopPanel/components/Path.jsx
+++ src/components/TopPanel/components/Path.jsx
@@ -3,31 +3,59 @@ import { useParams, Link } from "react-router-dom"
 
 import './Path.scss'
 
-function Path({path}) {
-    const {category} = useParams()
-    
+function Path({ path }) {
+    const { category } = useParams()
+    const pathRef = React.useRef()
+
+    const handlerScrollPath = (event) => {
+        let modifier = 0
+        if (event.deltaMode == event.DOM_DELTA_PIXEL) {
+            modifier = 1
+            // иные режимы возможны в Firefox
+        } else if (event.deltaMode == event.DOM_DELTA_LINE) {
+            modifier = parseInt(getComputedStyle(pathRef.current).lineHeight)
+        } else if (event.deltaMode == event.DOM_DELTA_PAGE) {
+            modifier = pathRef.current.clientHeight
+        }
+        if (event.deltaY != 0) {
+            // замена вертикальной прокрутки горизонтальной
+            pathRef.current.scrollLeft += modifier * event.deltaY
+            event.preventDefault()
+        }
+    }
+
+    React.useEffect(() => {
+        pathRef.current.addEventListener('wheel', handlerScrollPath)
+
+        return () => pathRef.current.removeEventListener('wheel', handlerScrollPath)
+    }, [])
+
+    React.useEffect(() => {
+        pathRef.current.scrollLeft = 9999999
+    }, [path])
+
     return (
-        <nav className="path">
+        <nav className="path" ref={pathRef}>
             <ul>
-                { path.map(item => (
+                {path.map(item => (
                     <li key={item?._id}>
-                        { item?.parent ? (
+                        {item?.parent ? (
                             <>
                                 <div className="delimiter">&#10140;</div>
                                 <Link to={"/files/" + category + "/" + item._id}><div className="link">{item.name}</div></Link>
                             </>
                         ) : (
                             <Link to={"/files/" + category + "/" + item._id}>
-                                <div className="link" style={{padding: 3}}>
+                                <div className="link" style={{ padding: 3 }}>
                                     <div className="home">Главная</div>
                                 </div>
                             </Link>
-                        ) }
-                        
+                        )}
+
                     </li>
-                )) }
+                ))}
             </ul>
-            
+
         </nav>
     )
 }
