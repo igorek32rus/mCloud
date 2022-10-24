6a89ad0df56a4e2c2dc3ecae668c10bc001b238f
Igor Elizaroff <igor.elizaroff@yandex.ru>
2022-10-24 03:53:10 +0300

    Fixed share modal window

--- src/components/modalwindows/Share.jsx
+++ src/components/modalwindows/Share.jsx
@@ -4,16 +4,19 @@ import { URLS } from "../../constants"
 import useFetch from "../../hooks/useFetch"
 import useNotification from "../../hooks/useNotification"
 
-import copyImg from "../../images/copy.svg"
 import linkCloseImg from "../../images/link_slash.svg"
+import linkImg from "../../images/link.svg"
 
 function Share({items}) {
     const [inputLink, setInputLink] = React.useState("")
+    const [shared, setShared] = React.useState(false)
     const [createNotification] = useNotification()
 
     const fetch = useFetch()
 
     const handleShareBtn = () => {
+        if (!shared) return
+
         navigator.clipboard.writeText(inputLink).then(() => {
             createNotification({
                 title: `Буфер обмена`, 
@@ -33,10 +36,12 @@ function Share({items}) {
 
         if (fileInfo.link) {
             setInputLink(URLS.SHARE + "/" + fileInfo.link)
+            setShared(true)
             return
         }
 
         setInputLink("Общий доступ к файлу закрыт")
+        setShared(false)
     }
 
     const getShareInfoFile = React.useCallback(async () => {
@@ -52,10 +57,12 @@ function Share({items}) {
 
         if (fileInfo.link) {
             setInputLink(URLS.SHARE + "/" + fileInfo.link)
+            setShared(true)
             return
         }
 
         setInputLink("Общий доступ к файлу закрыт")
+        setShared(false)
     }, [])
 
     React.useEffect(() => {
@@ -65,9 +72,8 @@ function Share({items}) {
     return (
         <>
             <div style={{display: 'flex', alignItems: "center"}}>
-                <input type="text" value={inputLink} readOnly={true} />
-                <div className="imgBtn" style={{backgroundImage: `url(${copyImg})`, marginLeft: 5}} onClick={handleShareBtn}></div>
-                <div className="imgBtn" style={{backgroundImage: `url(${linkCloseImg})`, marginLeft: 5}} onClick={handleToggleShare}></div>
+                <input type="text" className="share_input" value={inputLink} readOnly={true} onClick={handleShareBtn} />
+                <div className="button_share" style={{backgroundImage: `url(${shared ? linkCloseImg : linkImg})`, marginLeft: 5}} onClick={handleToggleShare}></div>
             </div>
         </>
     )
